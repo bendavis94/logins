c39b7e1199f934d96996f0d7dde4d9170be9bae0
bendavis94 <bendavis96@protonmail.com>
2022-03-30 17:21:42 +0300

    ch

--- public/js/pages/mobile-profile.js
+++ public/js/pages/mobile-profile.js
@@ -1,1 +1,31 @@
-console.log('darknet')
+const intro = introJs();
+
+intro.setOptions({
+    steps: [
+        {
+            intro: 'Welcome to the profile page... Lets take a tour'
+        },
+        {
+            element: '#recentsales',
+            intro: 'Table below shows a list of recent purchases made'
+        },
+        {
+            element: '#notificationz',
+            intro: 'Below are general notifications'
+        },
+        {
+            element: '#faqz',
+            intro: 'Below are questions many people ask about Darknet'
+        },
+        {
+            element: '#questionz',
+            intro: 'Here you can ask a question you have about the site'
+        }
+    ]
+})
+
+intro.start()
+
+
+
+
