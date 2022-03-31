7ba50f08027e90ae882af446cb0f5ffc2f4c0d3d
bendavis94 <bendavis96@protonmail.com>
2022-03-31 11:48:01 +0300

    Updates

--- js/cart/bitcoin.js
+++ js/cart/bitcoin.js
@@ -6,16 +6,8 @@
   
       binance.onmessage = event => {
         let confirm = JSON.parse(event.data);
-        if(JSON.parse(localStorage.getItem('banklogs')).length == 2){
-          bitcoin.innerHTML = (localStorage.getItem('banktotal') / parseFloat(confirm.k.c)).toFixed(5);
-          bitcoin.style.textDecoration = 'line-through'
-          bitcoin.style.textDecorationStyle = 'double'
-          bitcoin2.innerHTML = ((localStorage.getItem('banktotal') * 0.75) / parseFloat(confirm.k.c)).toFixed(5);
-        }  
-        else if(JSON.parse(localStorage.getItem('banklogs')).length == 1){
           bitcoin.innerHTML = (localStorage.getItem('banktotal') / parseFloat(confirm.k.c)).toFixed(5);
           bitcoin2.innerHTML = (localStorage.getItem('banktotal') / parseFloat(confirm.k.c)).toFixed(5);
           bitcoin2.style.display = 'none'
-        }
       }
   });
--- js/invoice/notifications.js
+++ js/invoice/notifications.js
@@ -4,31 +4,20 @@ $(document).ready(function() {
 	let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@kline_1h');
 	var toastbitcoin = '';
 
-	if (JSON.parse(localStorage.getItem('banklogs')).length == 2) {
-		ws.onmessage = (event) => {
-			let stockObject = JSON.parse(event.data);
-			toastbitcoin = ((toast * 0.75) / (parseFloat(stockObject.k.c))).toFixed(5);
-		}
-	} else {
-		ws.onmessage = (event) => {
-			let stockObject = JSON.parse(event.data);
-			toastbitcoin = (toast / (parseFloat(stockObject.k.c))).toFixed(5);
-		}
+
+	ws.onmessage = (event) => {
+		let stockObject = JSON.parse(event.data);
+		toastbitcoin = (toast / (parseFloat(stockObject.k.c))).toFixed(5);
 	}
+
 
 	var i = -1;
 	var $toastlast;
 
 	var getMessage = function() {
-		if (JSON.parse(localStorage.getItem('banklogs')).length == 2) {
-			var msgs = [
-				toastbitcoin + " " + 'bitcoin confirmation' + " " + 'not detected , scan the bitcoin address and send $' + (Math.round(toast * 0.75)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-			];
-		} else {
-			var msgs = [
-				toastbitcoin + " " + 'bitcoin confirmation' + " " + 'not detected , scan the bitcoin address and send $' + toast.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-			];
-		}
+		var msgs = [
+			toastbitcoin + " " + 'bitcoin confirmation' + " " + 'not detected , scan the bitcoin address and send $' + toast.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+		];
 		i++;
 		if (i === msgs.length) {
 			i = 0;
