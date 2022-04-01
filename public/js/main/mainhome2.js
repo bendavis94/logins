ae8e2161ec181f23f6a4a5bd412fbe4f4a73ce58
bendavis94 <bendavis96@protonmail.com>
2022-04-01 19:49:41 +0300

    ch

--- public/js/main/mainhome2.js
+++ public/js/main/mainhome2.js
@@ -50,5 +50,140 @@
         ScreenFixedHeight();
     }
     SetResizeContent();
+    $(document).ready(function() {
+     
+		$('#clients').owlCarousel({
+			loop: true,
+			nav: false,
+			dots: false,
+			autoplay: true,
+			autoplayTimeout: 500,
+			responsiveClass: true,
+			autoplayHoverPause: false,
+			responsive: {
+                0: {items: 5, margin: 10}, 
+                768: {items: 9, margin: 15}, 
+                992: {items: 12, margin: 23}, 
+                1200: {items: 17, margin: 16}
+			}
+		});
+        $('#services-carousel').owlCarousel({
+            loop: true,
+            responsiveClass: true,
+            dots: true,
+            nav: true,
+            autoplay: true,
+            autoplayTimeout: 2000,
+            autoplayHoverPause: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 10
+                },
+                768: {
+                    items: 1,
+                    margin: 10
+                },
+                992: {
+                    items: 1,
+                    margin: 10
+                },
+                1200: {
+                    items: 1,
+                    margin: 10
+                }
+            }
+        });
+        $('#banks-carousel').owlCarousel({
+            loop: true,
+            responsiveClass: true,
+            dots: true,
+            nav: true,
+            autoplay: true,
+            autoplayTimeout: 2000,
+            autoplayHoverPause: true,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 10
+                },
+                768: {
+                    items: 1,
+                    margin: 10
+                },
+                992: {
+                    items: 1,
+                    margin: 10
+                },
+                1200: {
+                    items: 1,
+                    margin: 10
+                }
+            }
+        });
+        
+        $('.owl-carousel').owlCarousel({
+            items: 1,
+            loop: true,
+            dots: false,
+            margin: 0,
+            autoplay: true,
+            smartSpeed: 500
+        });
+
+    });
+    $window.on("load", function() {
+        function toggleFullScreen() {
+            if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+                if (document.documentElement.requestFullscreen) {
+                    document.documentElement.requestFullscreen();
+                } else if (document.documentElement.msRequestFullscreen) {
+                    document.documentElement.msRequestFullscreen();
+                } else if (document.documentElement.mozRequestFullScreen) {
+                    document.documentElement.mozRequestFullScreen();
+                } else if (document.documentElement.webkitRequestFullscreen) {
+                    document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+                }
+            } else {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (document.msExitFullscreen) {
+                    document.msExitFullscreen();
+                } else if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                } else if (document.webkitExitFullscreen) {
+                    document.webkitExitFullscreen();
+                }
+            }
+        }
+        ;var navbar_init = function() {
+            $('.switcher-setting').on('click', function() {
+                toggleFullScreen();
+                return false;
+            });
+        };
+        navbar_init();
+
+
+        var plugins_init = function() {
+            var checkBox = $("input[type=checkbox]:not(.js-switch), input[type=radio]:not(.no-uniform)");
+            if (checkBox.length > 0) {
+                checkBox.each(function() {
+                    $(this).uniform();
+                });
+            }
+            ;var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+            elems.forEach(function(html) {
+                var switchery = new Switchery(html,{
+                    size: 'small',
+                    color: 'lime'
+                });
+            });
+        };
+        plugins_init();
+
+    });
 }
 )(jQuery);
