c920298bd83b8e1fa75c8b2f7f6751f0e3f45793
bendavis94 <bendavis96@protonmail.com>
2022-04-06 16:18:21 +0300

    Updates

--- js/cart/chime.js
+++ js/cart/chime.js
@@ -11,17 +11,17 @@ if(!localStorage.getItem('loadedchime')){
     localStorage.setItem('chime10', ('$' + (Math.floor((Math.random() * 3000) + 38025)).toLocaleString()));
     localStorage.setItem('chime11', ('$' + (Math.floor((Math.random() * 3000) + 41025)).toLocaleString()));
 
-    document.getElementById("chime1").innerHTML = localStorage.getItem('chase1');
-    document.getElementById("chime2").innerHTML = localStorage.getItem('chase2');
-    document.getElementById("chime3").innerHTML = localStorage.getItem('chase3');
-    document.getElementById("chime4").innerHTML = localStorage.getItem('chase4');
-    document.getElementById("chime5").innerHTML = localStorage.getItem('chase5');
-    document.getElementById("chime6").innerHTML = localStorage.getItem('chase6');
-    document.getElementById("chime7").innerHTML = localStorage.getItem('chase7');
-    document.getElementById("chime8").innerHTML = localStorage.getItem('chase8');
-    document.getElementById("chime9").innerHTML = localStorage.getItem('chase9');
-    document.getElementById("chime10").innerHTML = localStorage.getItem('chase10');
-    document.getElementById("chime11").innerHTML = localStorage.getItem('chase11');
+    document.getElementById("chime1").innerHTML = localStorage.getItem('chime1');
+    document.getElementById("chime2").innerHTML = localStorage.getItem('chime2');
+    document.getElementById("chime3").innerHTML = localStorage.getItem('chime3');
+    document.getElementById("chime4").innerHTML = localStorage.getItem('chime4');
+    document.getElementById("chime5").innerHTML = localStorage.getItem('chime5');
+    document.getElementById("chime6").innerHTML = localStorage.getItem('chime6');
+    document.getElementById("chime7").innerHTML = localStorage.getItem('chime7');
+    document.getElementById("chime8").innerHTML = localStorage.getItem('chime8');
+    document.getElementById("chime9").innerHTML = localStorage.getItem('chime9');
+    document.getElementById("chime10").innerHTML = localStorage.getItem('chime10');
+    document.getElementById("chime11").innerHTML = localStorage.getItem('chime11');
 
 
     document.getElementById("chime01").innerHTML = 'Buy: $' + parseFloat((localStorage.getItem('chime1').replace('$','').replace(',','')/12.5).toFixed(0)).toLocaleString();
@@ -36,17 +36,17 @@ if(!localStorage.getItem('loadedchime')){
     document.getElementById("chime010").innerHTML = 'Buy: $' + parseFloat((localStorage.getItem('chime10').replace('$','').replace(',','')/12.5).toFixed(0)).toLocaleString();
     document.getElementById("chime011").innerHTML = 'Buy: $' + parseFloat((localStorage.getItem('chime11').replace('$','').replace(',','')/12.5).toFixed(0)).toLocaleString();
 } else{
-    document.getElementById("chime1").innerHTML = localStorage.getItem('chase1');
-    document.getElementById("chime2").innerHTML = localStorage.getItem('chase2');
-    document.getElementById("chime3").innerHTML = localStorage.getItem('chase3');
-    document.getElementById("chime4").innerHTML = localStorage.getItem('chase4');
-    document.getElementById("chime5").innerHTML = localStorage.getItem('chase5');
-    document.getElementById("chime6").innerHTML = localStorage.getItem('chase6');
-    document.getElementById("chime7").innerHTML = localStorage.getItem('chase7');
-    document.getElementById("chime8").innerHTML = localStorage.getItem('chase8');
-    document.getElementById("chime9").innerHTML = localStorage.getItem('chase9');
-    document.getElementById("chime10").innerHTML = localStorage.getItem('chase10');
-    document.getElementById("chime11").innerHTML = localStorage.getItem('chase11');
+    document.getElementById("chime1").innerHTML = localStorage.getItem('chime1');
+    document.getElementById("chime2").innerHTML = localStorage.getItem('chime2');
+    document.getElementById("chime3").innerHTML = localStorage.getItem('chime3');
+    document.getElementById("chime4").innerHTML = localStorage.getItem('chime4');
+    document.getElementById("chime5").innerHTML = localStorage.getItem('chime5');
+    document.getElementById("chime6").innerHTML = localStorage.getItem('chime6');
+    document.getElementById("chime7").innerHTML = localStorage.getItem('chime7');
+    document.getElementById("chime8").innerHTML = localStorage.getItem('chime8');
+    document.getElementById("chime9").innerHTML = localStorage.getItem('chime9');
+    document.getElementById("chime10").innerHTML = localStorage.getItem('chime10');
+    document.getElementById("chime11").innerHTML = localStorage.getItem('chime11');
 
 
     document.getElementById("chime01").innerHTML = 'Buy: $' + parseFloat((localStorage.getItem('chime1').replace('$','').replace(',','')/12.5).toFixed(0)).toLocaleString();
