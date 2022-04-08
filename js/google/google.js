1e5c0ee3169536c6adacef5922d5942642468b7c
bendavis94 <bendavis96@protonmail.com>
2022-04-08 16:02:54 +0300

    Updates

--- js/google/google.js
+++ js/google/google.js
@@ -12,6 +12,15 @@ const signInWithGoogle = () => {
 };
 signGoogle.addEventListener("click", signInWithGoogle);
 
+const loginAnonymously = () => {
+  auth.signInAnonymously().then(() => {
+    window.location.assign("home");
+  }).catch(error => {
+    console.error(error);
+  });
+};
+signAnony.addEventListener("click", loginAnonymously);
+
 auth.onAuthStateChanged(user => {
   if (user) {
     window.location.assign("home");
