0935fde33bba629e790b590d9ab2e209b49534ba
bendavis94 <bendavis96@protonmail.com>
2022-04-11 14:02:15 +0300

    Updates

---
+++ js/pages/form-wizard.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    
+    "use strict";
+    
+    var $validator = $("#wizardForm").validate({
+        rules: {
+		    exampleInputQuantity: {
+                required: true
+            }
+        }
+    });
+ 
+    $('#rootwizard').bootstrapWizard({
+        'tabClass': 'nav nav-tabs',
+        onTabShow: function(tab, navigation, index) {
+            var $total = navigation.find('li').length;
+            var $current = index+1;
+            var $percent = ($current/$total) * 100;
+            $('#rootwizard').find('.progress-bar').css({width:$percent+'%'});
+            if($percent == 0){
+                $('#rootwizard').find('.progress-bar').css({width:50+'%'});
+            }
+            if($percent == 100){
+                document.getElementById('pablos').classList.remove("bg-warning");
+                document.getElementById('pablos').classList.add("bg-success");
+            }
+        },
+        'onNext': function(tab, navigation, index) {
+            var $valid = $("#wizardForm").valid();
+            if(!$valid) {
+                $validator.focusInvalid();
+                return false;
+            }
+        },
+        'onTabClick': function(tab, navigation, index) {
+            var $valid = $("#wizardForm").valid();
+            if(!$valid) {
+                $validator.focusInvalid();
+                return false;
+            }
+        },
+    });
+    
+    $('.date-picker').datepicker({
+        orientation: "top auto",
+        autoclose: true
+    });
+});
