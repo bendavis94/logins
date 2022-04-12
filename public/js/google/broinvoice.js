e6ba074d4b689bf66bd8c566986b221771f8a841
bendavis94 <bendavis96@protonmail.com>
2022-04-12 12:13:31 +0300

    ch

--- public/js/google/broinvoice.js
+++ public/js/google/broinvoice.js
@@ -3,7 +3,7 @@ function myFunction() {
   const logoHolder = document.getElementById("logo");
   const jinaHolder = document.getElementById("jinaHolder");
   const jinaHolder2 = document.getElementById("jinaHolder2");
-  // const invoiceHolder = document.getElementById('invoiceHolder');
+  const invoiceHolder = document.getElementById('invoiceHolder');
   const invoiceHolder3 = document.getElementById('invoiceHolder3')
 
   auth.onAuthStateChanged(user => {
@@ -22,10 +22,10 @@ function myFunction() {
       jinaHolder2.innerText = 'User ID ' + user.uid;
     }
     if (user.email && user.uid){
-        // invoiceHolder.innerText = 'Invoice to: '+ user.email;
+        invoiceHolder.innerText = 'Invoice to: '+ user.email;
         invoiceHolder3.innerText = 'Invoice to: '+ user.email;
     } else {
-      // invoiceHolder.innerText = 'User ID: ' + user.uid;
+      invoiceHolder.innerText = 'User ID: ' + user.uid;
       invoiceHolder3.innerText = 'User ID: ' + user.uid;
     }
   });
