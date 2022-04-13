f30aaa14c198cf9314ab5b5f1a27f9d4426d6dcb
bendavis94 <bendavis96@protonmail.com>
2022-04-13 19:05:57 +0300

    ch

--- public/js/cart/shopi.js
+++ public/js/cart/shopi.js
@@ -44,99 +44,6 @@ if(localStorage.getItem('banklogs') && ((JSON.parse(localStorage.getItem('banklo
         ]).draw();
     });
 
-    for(var i = 0; i < items.length; i++) {
-        var cartRow2 = document.createElement('li');
-        cartRow2.classList.add('bg-black','champez3li');
-
-        var cartRow3 = document.createElement('div');
-        cartRow3.classList.add('col-lg-3', 'col-lg-3', 'col-xl-2', 'col-md-4', 'col-6');
-
-        var balance2 = items[i].balance;
-        var price2 = items[i].price;
-        var balance3 = balance2.replace('Balance: ', '');
-        var price3 = price2.replace('Price: ', 'In Cart: ');
-
-        var cartItems2 = document.getElementsByClassName('mobile1')[0];
-        var cartItems3 = document.getElementsByClassName('xenon3')[0];
-
-        var cartRowContents2 = `
-            <table class="table table-bordered table-mobile">
-                <thead>
-                    <tr>
-                        <th>
-                            <img src=${items[i].image} class="champez3img">
-                        </th>
-                        <th class="btn-balance">
-                            ${items[i].balance}
-                        </th>
-                        <th class="btn-price">
-                            ${items[i].price}
-                        </th>
-                        <th>
-                            <img src="failure.svg" class="champez3fail">
-                        </th>
-                    </tr>
-                </thead>
-            </table>
-            <table class="table table-bordered table-mobile">
-                <thead>
-                    <tr>
-                        <th>${items[i].account}</th>
-                    </tr>
-                </thead>
-            </table>
-            <table class="table table-bordered table-mobile">
-                <tbody>
-                    <tr>
-                        <td>${items[i].website}</td>
-                        <td>${items[i].info1}</td>
-                    </tr>
-                    <tr>
-                        <td>${items[i].info2}</td>
-                        <td>${items[i].info3}</td>
-                    </tr>
-                    <tr>
-                        <td>${items[i].info4}</td>
-                        <td>${items[i].info5}</td>
-                    </tr>
-                    <tr>
-                        <td>${items[i].info6}</td>
-                        <td>${items[i].info7}</td>
-                    </tr>
-                </tbody>
-            </table>
-        `;
-        var cartRowContents3 = `
-            <div class="feature-box-04">
-                <div class="no-padding border">
-                    <div>
-                        <p>${balance3}</p>
-                        <img src=${items[i].image} class="borderp">
-                    </div>
-                    <p>${items[i].info1}</p>
-                    <p>${items[i].info2}</p>
-                    <p>${items[i].info3}</p>
-                    <p>${items[i].info4}</p>
-                    <p>${items[i].info5}</p>
-                    <p>${items[i].info6}</p>
-                    <p class="nth9">${items[i].info7}</p>
-                    <p>${items[i].account}</p>
-                    <button type="submit" class="butn medium">     
-                        ${price3}
-                    </button>
-                </div>
-            </div>
-        `;
-        cartRow2.innerHTML = cartRowContents2;
-        cartRow3.innerHTML = cartRowContents3;
-
-        cartItems2.prepend(cartRow2);
-        cartItems3.prepend(cartRow3);
-
-        cartRow2.getElementsByClassName('champez3fail')[0].addEventListener('click', removeCartItem2);
-
-    }
-
     updateCartTotal();
 
     var removeFromCartButtons = document.getElementsByClassName('btn-remove');
@@ -168,32 +75,9 @@ function removeCartItem(event) {
     var info7 = cartItem.children[12].innerText;
     removeItemFromCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
     buttonClicked.parentElement.parentElement.remove();
+    localStorage.setItem('time-left',600);
     updateCartTotal()
 }
-
-
-function removeCartItem2(event){
-    var buttonClicked = event.target;
-    var cartItem = buttonClicked.parentElement.parentElement.parentElement.parentElement.parentElement;
-    var price =   cartItem.children[0].children[0].children[0].children[2].innerText;
-    var balance = cartItem.children[0].children[0].children[0].children[1].innerText;
-    var image =   cartItem.children[0].children[0].children[0].children[0].children[0].src;
-
-    var account = cartItem.children[1].children[0].children[0].children[0].innerText;
-    var website = cartItem.children[2].children[0].children[0].children[0].innerText;
-    var info1 =   cartItem.children[2].children[0].children[0].children[1].innerText;
-    var info2 =   cartItem.children[2].children[0].children[1].children[0].innerText;
-    var info3 =   cartItem.children[2].children[0].children[1].children[1].innerText;
-    var info4 =   cartItem.children[2].children[0].children[2].children[0].innerText;
-    var info5 =   cartItem.children[2].children[0].children[2].children[1].innerText;
-    var info6 =   cartItem.children[2].children[0].children[3].children[0].innerText;
-    var info7 =   cartItem.children[2].children[0].children[3].children[1].innerText;
-
-    removeItemFromCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
-    buttonClicked.parentElement.parentElement.remove();
-    updateCartTotal()
-}
-
 
 
 function removeItemFromCart(price, balance,account,website,image,info1,info2,info3,info4,info5,info6,info7){
