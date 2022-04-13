ac870146c79942aa6a0eb3e31bd61e6058a28969
bendavis94 <bendavis96@protonmail.com>
2022-04-13 09:07:06 +0300

    ch

--- public/js/google/broprofile.js
+++ public/js/google/broprofile.js
@@ -28,13 +28,7 @@ function myFunction() {
 			rockHolder.innerText = user.displayName;
 			tableidHolder.value = "Name: " + user.displayName;
 			// idHolder3.innerText = 'User ID ' + user.uid;
-		} else {
-			jinaHolder.innerText = "Anonymous";
-			jinaHolder2.innerText = 'User ID ' + user.uid;
-			rockHolder.innerText = "Anonymous";
-			tableidHolder.value = "Name: Anonymous";
-			// idHolder3.innerText = 'User ID ' + user.uid;
-		  }
+		}
 		if (user.uid) {
 			uidHolder.innerText = user.uid;
 		}
@@ -42,11 +36,7 @@ function myFunction() {
 			email1.innerHTML = `Check your email spam folder @:<strong>${user.email}</strong> after buying a bank log`;
 			email2.innerHTML = `Cashout Method is also sent to your email address @:<strong>${user.email}</strong>`;
 			email5.innerHTML = user.email;
-		} else {
-			email1.innerHTML = `Bank Log files + cookies can only be downloaded once, so make sure you save them in a folder you won't forget.`
-			email2.innerHTML = `Use a winrar software to extract bank log files from .zip format after a successful download,... Also Download Cookies Editor extension for chrome browser and use it to import cookies`;
-			email5.innerHTML = 'No email connected';
-		}
+		} 
 
 		let goodies = [];
 
