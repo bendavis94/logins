89967ffb51f6eef1edd20f84236306e4867703f9
bendavis94 <bendavis96@protonmail.com>
2022-04-15 12:46:30 +0300

    Updates

--- js/main/maininvoice.js
+++ js/main/maininvoice.js
@@ -81,5 +81,54 @@
         var $gallery = $('.gallery').isotope({});
         $window.stellar();
     });
+
+    function toggleFullScreen() {
+        if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+            if (document.documentElement.requestFullscreen) {
+                document.documentElement.requestFullscreen();
+            } else if (document.documentElement.msRequestFullscreen) {
+                document.documentElement.msRequestFullscreen();
+            } else if (document.documentElement.mozRequestFullScreen) {
+                document.documentElement.mozRequestFullScreen();
+            } else if (document.documentElement.webkitRequestFullscreen) {
+                document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+            }
+        } else {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            }
+        }
+    }
+    ;var navbar_init = function() {
+        $('.switcher-setting').on('click', function() {
+            toggleFullScreen();
+            return false;
+        });
+    };
+    navbar_init();
+
+
+    var plugins_init = function() {
+        var checkBox = $("input[type=checkbox]:not(.js-switch), input[type=radio]:not(.no-uniform)");
+        if (checkBox.length > 0) {
+            checkBox.each(function() {
+                $(this).uniform();
+            });
+        }
+        ;var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+        elems.forEach(function(html) {
+            var switchery = new Switchery(html,{
+                size: 'small',
+                color: 'lime'
+            });
+        });
+    };
+    plugins_init();
 }
 )(jQuery);
