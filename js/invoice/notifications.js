88adc73023cb8f286e2d1159b2c055c1cf01ab45
bendavis94 <bendavis96@protonmail.com>
2022-04-17 13:56:38 +0300

    Updates

--- js/invoice/notifications.js
+++ js/invoice/notifications.js
@@ -121,7 +121,7 @@
 
 	$(toastbut_2).click(function() {
 		var shortCutFunction = 'success';
-		var msg = '$100 bitcoin confirmation not detected to complete download, do not close this page';
+		var msg = '$100 bitcoin confirmation not detected to complete download, do not close this page or clear your browsing history';
 		var title = '';
 		toastr.options = {
 			closeButton: true,
