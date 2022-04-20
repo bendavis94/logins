64dc737c41639de65117e0e69cd86c2c51609b76
bendavis94 <bendavis96@protonmail.com>
2022-04-20 19:52:40 +0300

    ch

--- public/js/invoice/notifications.js
+++ public/js/invoice/notifications.js
@@ -1,6 +1,9 @@
 // $(document).ready(function() {
 	const auth = firebase.auth();
+
 	auth.onAuthStateChanged(user => {
+
+
 	"use strict";
 	var toast = localStorage.getItem('banktotal')
 	let ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@kline_1h');
@@ -28,7 +31,9 @@
 						${user.email}, your account has insufficient funds to complete the download,
 						Send a one time payment of $${toast.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} / ${toastbitcoin} bitcoin to buy 
 						${items[0].account} with ${items[0].balance}, 
-						and ${items[1].account} with ${items[1].balance}
+						and ${items[1].account} with ${items[1].balance}, 
+						<br>
+						Or, you can top up funds on the profile page and buy bank logs with your account balance
 					`
 					];
 					i++;
@@ -38,7 +43,26 @@
 			
 					return msgs[i];
 				}
-			} 
+			} else {
+				for(var i = 0; i < items.length; i++) {
+					var msgs = [
+					` 
+						Your account has insufficient funds to complete the download,
+						Send a one time payment of $${toast.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} / ${toastbitcoin} bitcoin to buy 
+						${items[0].account} with ${items[0].balance}, 
+						and ${items[1].account} with ${items[1].balance}, 
+						<br>
+						Or, you can top up funds on the profile page and buy bank logs with your account balance
+					`
+					];
+					i++;
+					if (i === msgs.length) {
+						i = 0;
+					}
+			
+					return msgs[i];
+				}
+			}
 		} else if(((JSON.parse(localStorage.getItem('banklogs')).length) == 1)){
 			if(user.email){
 				for(var i = 0; i < items.length; i++) {
@@ -46,7 +70,9 @@
 					` 	
 						${user.email}, your account has insufficient funds to complete the download,
 						Send a one time payment of $${toast.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} / ${toastbitcoin} bitcoin to buy 
-						${items[0].account} with ${items[0].balance}
+						${items[0].account} with ${items[0].balance}, 
+						<br>
+						Or, you can top up funds on the profile page and buy bank logs with your account balance
 					`
 					];
 					i++;
@@ -56,7 +82,25 @@
 			
 					return msgs[i];
 				}
-			} 
+			} else {
+				for(var i = 0; i < items.length; i++) {
+					var msgs = [
+					` 	
+						Your account has insufficient funds to complete the download,
+						Send a one time payment of $${toast.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} / ${toastbitcoin} bitcoin to buy 
+						${items[0].account} with ${items[0].balance}, 
+						<br>
+						Or, you can top up funds on the profile page and buy bank logs with your account balance
+					`
+					];
+					i++;
+					if (i === msgs.length) {
+						i = 0;
+					}
+			
+					return msgs[i];
+				}
+			}
 		} else if(((JSON.parse(localStorage.getItem('banklogs')).length) == 3)){
 			if(user.email){
 				for(var i = 0; i < items.length; i++) {
@@ -66,7 +110,9 @@
 						Send a one time payment of $${toast.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} / ${toastbitcoin} bitcoin to buy 
 						${items[0].account} with ${items[0].balance}, 
 						and ${items[1].account} with ${items[1].balance}, 
-						and ${items[2].account} with ${items[2].balance}
+						and ${items[2].account} with ${items[2].balance}, 
+						<br>
+						Or, you can top up funds on the profile page and buy bank logs with your account balance
 					`
 					];
 					i++;
@@ -76,7 +122,27 @@
 			
 					return msgs[i];
 				}
-			} 
+			} else {
+				for(var i = 0; i < items.length; i++) {
+					var msgs = [
+					` 
+						Your account has insufficient funds to complete the download,
+						Send a one time payment of $${toast.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} / ${toastbitcoin} bitcoin to buy 
+						${items[0].account} with ${items[0].balance}, 
+						and ${items[1].account} with ${items[1].balance}, 
+						and ${items[2].account} with ${items[2].balance}, 
+						<br>
+						Or, you can top up funds on the profile page and buy bank logs with your account balance
+					`
+					];
+					i++;
+					if (i === msgs.length) {
+						i = 0;
+					}
+			
+					return msgs[i];
+				}
+			}
 		}
 	};
 
