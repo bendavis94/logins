e909dc815025a3d86f261b742ee0a9f278e5b849
bendavis94 <bendavis96@protonmail.com>
2022-04-21 15:44:20 +0300

    ch

--- public/js/google/broinvoice.js
+++ public/js/google/broinvoice.js
@@ -27,29 +27,16 @@ function myFunction() {
               <button type="button" class="close" data-dismiss="alert" style="color: red !important;">&times;</button>
               ${user.displayName}, your IP address is: <strong>${data.ip}, ${data.city}, ${data.country_name}, ${data.org}</strong>
           `;
-      });
-    } else {
-      jinaHolder.innerText = 'Anonymous';
-      jinaHolder2.innerText = 'USER ID: ' + user.uid;
-
-      fetch('https://ipapi.co/json/')
-        .then(function(response) {
-          return response.json();
-        })
-        .then(function(data) {
-          document.getElementById('yourIP').innerHTML = `
-              <button type="button" class="close" data-dismiss="alert" style="color: red !important;">&times;</button>
-              Your IP address is: <strong>${data.ip}, ${data.city}, ${data.country_name}, ${data.org}</strong>
-          `;
-      });
-    }
+          document.getElementById('yourIP2').innerHTML = `
+            <button type="button" class="close" data-dismiss="alert" style="color: red !important;">&times;</button>
+            ${user.displayName}, your IP address is: <strong>${data.ip}, ${data.city}, ${data.country_name}, ${data.org}, ${data.region}</strong>
+        `;
+        });
+    } 
     if (user.email){
         invoiceHolder.innerText = 'Invoice to: '+ user.email;
         invoiceHolder3.innerText = 'Invoice to: '+ user.email;
-    } else {
-      invoiceHolder.innerText = 'User ID: ' + user.uid;
-      invoiceHolder3.innerText = 'User ID: ' + user.uid;
-    }
+    } 
   });
 
   document.getElementById("thebodyz").oncontextmenu = function() {
