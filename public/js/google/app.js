3053fa4c9f39818f54506c572ae8f9ad5a773c13
bendavis94 <bendavis96@protonmail.com>
2022-04-22 15:44:08 +0300

    ch

--- public/js/google/app.js
+++ public/js/google/app.js
@@ -1,16 +1,3 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyBBj87G9GauwrVebZO2XtrT-2V_SzL_uAI",
-    authDomain: "firegram-3657b.firebaseapp.com",
-    projectId: "firegram-3657b",
-    storageBucket: "firegram-3657b.appspot.com",
-    messagingSenderId: "111342370399",
-    appId: "1:111342370399:web:50e4219a24340b619265ea",
-    measurementId: "G-RZMX1M184E"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 document.getElementById('file').addEventListener('change', (event) => {
     const file = event.target.files[0];
     const storageRef = firebase.storage().ref('images' + file.name);
@@ -26,11 +13,11 @@ document.getElementById('file').addEventListener('change', (event) => {
         const url = await storageRef.getDownloadURL();
 
         var cartRow = document.createElement('div');
-        cartRow.classList.add('col-lg-2');
+        cartRow.classList.add('col-xl-2','col-lg-3','col-md-4','col-6');
         var cartItems = document.getElementById('list');
 
         var cartRowContents = `
-            <img src=${url} />
+            <img src=${url} style="margin-bottom: 10px"/>
         `;
 
         cartRow.innerHTML = cartRowContents;
@@ -39,12 +26,9 @@ document.getElementById('file').addEventListener('change', (event) => {
         console.log(url);
 
     });
-
 });
 
-
 var storageRef2 = firebase.storage().ref();
-
 var i = 0;
 
 storageRef2.child('/').listAll().then(function(result){
@@ -60,11 +44,11 @@ function displayImage(row, images){
         console.log(url);
 
         var cartRow = document.createElement('div');
-        cartRow.classList.add('col-lg-2');
+        cartRow.classList.add('col-xl-2','col-lg-3','col-md-4','col-6','items');
         var cartItems = document.getElementById('list');
 
         var cartRowContents = `
-            <img src=${url}/>
+            <img src=${url} style="margin-bottom: 10px"/>
         `;
 
         cartRow.innerHTML = cartRowContents;
