1c81c8e99d3432c52d15804b3f99adc0ac6cfcc2
bendavis94 <bendavis96@protonmail.com>
2022-04-23 12:06:57 +0300

    Updates

--- js/google/bro.js
+++ js/google/bro.js
@@ -5,6 +5,7 @@ function myFunction() {
   const jinaHolder2 = document.getElementById("jinaHolder2");
 
   auth.onAuthStateChanged(user => {
+    console.log(user);
     if (!user) {
       window.location.assign("index");
     }
