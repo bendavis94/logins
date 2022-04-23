053d969aa55f2896be5fd800026e2669c73d58df
bendavis94 <bendavis96@protonmail.com>
2022-04-23 11:24:47 +0300

    ch

--- public/js/google/google.js
+++ public/js/google/google.js
@@ -1,6 +1,4 @@
 const signGoogle = document.getElementById("signGoogle");
-const signAnony = document.getElementById("signAnony");
-// const signInWithFacebookButton = document.getElementById('signInWithFacebook');
 
 const auth = firebase.auth();
 const signInWithGoogle = () => {
@@ -12,28 +10,6 @@ const signInWithGoogle = () => {
   });
 };
 signGoogle.addEventListener("click", signInWithGoogle);
-
-// const signInWithFacebook = () => {
-//   const facebookProvider = new firebase.auth.FacebookAuthProvider();
-
-//   auth.signInWithPopup(facebookProvider)
-//   .then(() => {
-//     window.location.assign('home');
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   })
-// }
-// signInWithFacebookButton.addEventListener('click', signInWithFacebook);
-
-const loginAnonymously = () => {
-  auth.signInAnonymously().then(() => {
-    window.location.assign("home");
-  }).catch(error => {
-    console.error(error);
-  });
-};
-signAnony.addEventListener("click", loginAnonymously);
 
 auth.onAuthStateChanged(user => {
   if (user) {
