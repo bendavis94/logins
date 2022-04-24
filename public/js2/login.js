38c54fcb4bc942bdfbb134ac27b9995e59fcb068
bendavis94 <bendavis96@protonmail.com>
2022-04-24 15:38:19 +0300

    ch

--- public/js2/login.js
+++ public/js2/login.js
@@ -14,32 +14,11 @@ const signUpFunction = () => {
     .then(() => {
         console.log('Link Sent Successfully');
         window.localStorage.setItem('emailForSignIn', email);
-        // var emailSent = true;
     })
     .catch(error => {
         console.error(error);
-    })
+    });
 }
-
-
-if (auth.isSignInWithEmailLink(window.location.href)) {
-    var email = window.localStorage.getItem('emailForSignIn');
-    if (!email) {
-      email = window.prompt('Please provide your email for confirmation');
-    }
-    auth.signInWithEmailLink(email, window.location.href)
-      .then((result) => {
-        console.log('Signed in successfully')
-        // window.localStorage.removeItem('emailForSignIn');
-        // window.location.assign('home')
-      })
-      .catch((error) => {
-        console.log('An error occurred')
-      });
-}
-
-// auth.signInWithEmailLink
-
 
 signUp.addEventListener('click', signUpFunction);
 
