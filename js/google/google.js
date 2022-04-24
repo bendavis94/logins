52e0bb3aacc101fb605a490fa23b390cab8ea619
bendavis94 <bendavis96@protonmail.com>
2022-04-24 14:36:09 +0300

    Updates

--- js/google/google.js
+++ js/google/google.js
@@ -20,7 +20,7 @@ const signInWithGoogle = () => {
   const googleProvider = new firebase.auth.GoogleAuthProvider;
   auth.signInWithPopup(googleProvider).then(() => {
     sendVerificationEmail();
-    window.location.assign("home");
+    // window.location.assign("home");
   }).catch(error => {
     console.error(error);
   });
@@ -32,7 +32,7 @@ const signInWithGithub = () => {
   const githubProvider = new firebase.auth.GithubAuthProvider;
   auth.signInWithPopup(githubProvider).then(() => {
     sendVerificationEmail();
-    window.location.assign("home");
+    // window.location.assign("home");
   }).catch(error => {
     console.error(error);
   })
@@ -43,7 +43,7 @@ const signInWithYahoo = () => {
   const yahooProvider = new firebase.auth.OAuthProvider('yahoo.com');
   auth.signInWithPopup(yahooProvider).then(() => {
     sendVerificationEmail();
-    window.location.assign("home");
+    // window.location.assign("home");
   }).catch(error => {
     console.error(error);
   })
@@ -52,8 +52,10 @@ signYahoo.addEventListener("click", signInWithYahoo);
 
 
 auth.onAuthStateChanged(user => {
-  if (user) {
+  if (user.emailVerified) {
     window.location.assign("home");
+  } else {
+    console.log("Not yet verified")
   }
 });
 
