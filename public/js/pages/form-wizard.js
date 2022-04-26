96914cd492df8728cbac7989d57021ae96ebfb0d
bendavis94 <bendavis96@protonmail.com>
2022-04-26 12:18:20 +0300

    ch

--- public/js/pages/form-wizard.js
+++ public/js/pages/form-wizard.js
@@ -2,21 +2,34 @@ $(document).ready(function() {
     "use strict";
     var $validator = $("#wizardForm").validate({
         rules: {
-		    exampleInputEmail: {
+            exampleInputName: {
                 required: true,
-                email: true
-		    }
+                number: true
+            }
         }
     });
  
     $('#rootwizard').bootstrapWizard({
-        'onNext': function() {
+        'tabClass': 'nav nav-tabs',
+        onTabShow: function(tab, navigation, index) {
+            var $total = navigation.find('li').length;
+            var $current = index+1;
+            var $percent = ($current/$total) * 100;
+            $('#rootwizard').find('.progress-bar').css({width:$percent+'%'});
+        },
+        'onNext': function(tab, navigation, index) {
             var $valid = $("#wizardForm").valid();
             if(!$valid) {
                 $validator.focusInvalid();
                 return false;
             }
-        }
+        },
+        'onTabClick': function(tab, navigation, index) {
+            var $valid = $("#wizardForm").valid();
+            if(!$valid) {
+                $validator.focusInvalid();
+                return false;
+            }
+        },
     });
-
 });
