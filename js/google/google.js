b2e4f0d5a2e7595612a126ebac5c9f0e5598ebaa
bendavis94 <bendavis96@protonmail.com>
2022-04-26 10:39:45 +0300

    Updates

--- js/google/google.js
+++ js/google/google.js
@@ -1,6 +1,6 @@
 const mailField = document.getElementById('exampleInputEmail');
 const signUp = document.getElementById('signUp');
-const signFacebook = document.getElementById("signFacebook");
+// const signFacebook = document.getElementById("signFacebook");
 const signGoogle = document.getElementById("signGoogle");
 const signGithub = document.getElementById("signGithub");
 const signYahoo = document.getElementById("signYahoo");
@@ -54,16 +54,16 @@ if (auth.isSignInWithEmailLink(window.location.href)) {
     });
 }
 
-const signInWithFacebook = () => {
-  const facebookProvider = new firebase.auth.FacebookAuthProvider;
-  auth.signInWithPopup(facebookProvider).then(() => {
-    sendVerificationEmail();
-    window.location.assign("home");
-  }).catch(error => {
-    console.error(error);
-  });
-};
-signFacebook.addEventListener("click", signInWithFacebook);
+// const signInWithFacebook = () => {
+//   const facebookProvider = new firebase.auth.FacebookAuthProvider;
+//   auth.signInWithPopup(facebookProvider).then(() => {
+//     sendVerificationEmail();
+//     window.location.assign("home");
+//   }).catch(error => {
+//     alert(error.message);
+//   });
+// };
+// signFacebook.addEventListener("click", signInWithFacebook);
 
 const signInWithGoogle = () => {
   const googleProvider = new firebase.auth.GoogleAuthProvider;
