5d0cf00f5fedab05c8262b15404eb5811ba8ca74
bendavis94 <bendavis96@protonmail.com>
2022-04-28 00:31:03 +0300

    ch

--- public/js/pages/home-modal.js
+++ public/js/pages/home-modal.js
@@ -1,9 +1,3 @@
 $("#exampleModal").on("show.bs.modal", function (event) {
     ("use strict");
 });
-
-$('#myform').on('submit', function(ev) {
-    $('#codeModal').modal('show');
-    $('#phoneModal').modal('hide');
-    ev.preventDefault();
-});
