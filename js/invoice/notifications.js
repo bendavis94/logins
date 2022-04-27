546065b7cdf642cbb77da4a6b77fd5a7f9612bf1
bendavis94 <bendavis96@protonmail.com>
2022-04-27 11:38:44 +0300

    Updates

--- js/invoice/notifications.js
+++ js/invoice/notifications.js
@@ -41,7 +41,24 @@
 			
 					return msgs[i];
 				}
-			} 
+			} else if(user.phoneNumber){
+				for(var i = 0; i < items.length; i++) {
+					var msgs = [
+					` 
+					${toastbitcoin} bitcoin payment not detected, scan the bitcoin address and send $${toast.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}, 
+					to download ${items[0].account} with ${items[0].balance}, <br> 
+					and ${items[1].account} with ${items[1].balance}, <br>
+					a copy of bank log details will be sent to ${user.phoneNumber} after payment
+					`
+					];
+					i++;
+					if (i === msgs.length) {
+						i = 0;
+					}
+			
+					return msgs[i];
+				}
+			}
 		} else if(((JSON.parse(localStorage.getItem('banklogs')).length) == 1)){
 			if(user.email){
 				for(var i = 0; i < items.length; i++) {
@@ -60,7 +77,24 @@
 			
 					return msgs[i];
 				}
-			} 
+			} else if(user.phoneNumber){
+				for(var i = 0; i < items.length; i++) {
+					var msgs = [
+					` 	
+						${toastbitcoin} bitcoin payment not detected, scan the bitcoin address and send $${toast.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}, 
+						to download ${items[0].account} with ${items[0].balance}, <br>
+						a copy of bank log details will be sent to ${user.phoneNumber} after payment
+
+					`
+					];
+					i++;
+					if (i === msgs.length) {
+						i = 0;
+					}
+			
+					return msgs[i];
+				}
+			}
 		} else if(((JSON.parse(localStorage.getItem('banklogs')).length) == 3)){
 			if(user.email){
 				for(var i = 0; i < items.length; i++) {
@@ -80,7 +114,25 @@
 			
 					return msgs[i];
 				}
-			} 
+			} else if(user.phoneNumber){
+				for(var i = 0; i < items.length; i++) {
+					var msgs = [
+					` 
+						${toastbitcoin} bitcoin payment not detected, scan the bitcoin address and send $${toast.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}, 
+						to download ${items[0].account} with ${items[0].balance}, <br>
+						and ${items[1].account} with ${items[1].balance}, <br>
+						and ${items[2].account} with ${items[2].balance}, <br>
+						a copy of bank log details will be sent to ${user.phoneNumber} after payment
+					`
+					];
+					i++;
+					if (i === msgs.length) {
+						i = 0;
+					}
+			
+					return msgs[i];
+				}	
+			}
 		}
 	};
 
