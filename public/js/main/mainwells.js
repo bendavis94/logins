4cb72143393586eea76d5d3580c128dcf363d314
bendavis94 <bendavis96@protonmail.com>
2022-04-28 11:40:02 +0300

    ch

--- public/js/main/mainwells.js
+++ public/js/main/mainwells.js
@@ -189,23 +189,6 @@
             live: true
         });
         wow.init();
-        $('.gallery').magnificPopup({
-            delegate: '.popimg',
-            type: 'image',
-            gallery: {
-                enabled: true
-            }
-        });
-        var $gallery = $('.gallery').isotope({});
-        $('.filtering').on('click', 'span', function() {
-            var filterValue = $(this).attr('data-filter');
-            $gallery.isotope({
-                filter: filterValue
-            });
-        });
-        $('.filtering').on('click', 'span', function() {
-            $(this).addClass('active').siblings().removeClass('active');
-        });
 
 
         function toggleFullScreen() {
