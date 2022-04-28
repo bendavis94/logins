37b767fcfcff13210f0ed553346610619a047b39
bendavis94 <bendavis96@protonmail.com>
2022-04-28 22:34:08 +0300

    ch

--- public/js/main.js
+++
@@ -1,9 +0,0 @@
-! function(g) {
-    "use strict";
-    var c = g(window);
-    c.on("load", function() {
-        g(".portfolio-gallery,.portfolio-gallery-isotope").lightGallery(), g(".portfolio-link").on("click", e => {
-            e.stopPropagation()
-        })
-    })
-}(jQuery);
