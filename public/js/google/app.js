41230f188adcdd000335da4e81c951a254aba471
bendavis94 <bendavis96@protonmail.com>
2022-04-29 12:03:23 +0300

    ch

--- public/js/google/app.js
+++ public/js/google/app.js
@@ -1,7 +1,6 @@
 document.getElementById('file').addEventListener('change', (event) => {
     const file = event.target.files[0];
     const storageRef = firebase.storage().ref('images' + file.name);
-
     storageRef.put(file).on('state_changed', (snapshot) => {
         const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         const progressBar_2 = document.getElementById("pablos");
@@ -19,18 +18,12 @@ document.getElementById('file').addEventListener('change', (event) => {
         `;
         cartRow.innerHTML = cartRowContents;
         cartItems.prepend(cartRow);
-
-        // console.log(url);
-
     });
 });
-
 var storageRef2 = firebase.storage().ref();
 var i = 0;
-
 storageRef2.child('/').listAll().then(function(result){
     result.items.forEach(function(imageRef){
-        // console.log("Image reference" + imageRef.toString());
         i++;
         displayImage(i, imageRef);
     })
@@ -38,7 +31,6 @@ storageRef2.child('/').listAll().then(function(result){
 
 function displayImage(row, images){
     images.getDownloadURL().then(function(url){
-        // console.log(url);
         var cartRow = document.createElement('div');
         cartRow.classList.add('col-xl-2','col-lg-3','col-md-4','col-6','items');
         var cartItems = document.getElementById('list');
