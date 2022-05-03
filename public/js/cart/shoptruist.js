b77b2004ff7612a7804a6c78300fd388e05aea5a
bendavis94 <bendavis96@protonmail.com>
2022-05-03 08:29:13 +0300

    ch

--- public/js/cart/shoptruist.js
+++ public/js/cart/shoptruist.js
@@ -257,6 +257,22 @@ function ad0012(event) {
     addItemToCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
 }
 
+function ad0013(event) {
+    var price = 'Price: $280';
+    var balance = 'Balance: $14,015';
+    var website = 'bankofamerica.com';
+    var info1 = 'Username + Password';
+    var info2 = 'Account& Routing No';
+    var info3 = 'Date of Birth';
+    var info4 = 'Social Security No';
+    var info5 = 'ClientIP+ UserAgent';
+    var info6 = 'Name + Email Access';
+    var info7 = 'Owner Complete Fullz';
+    var image = 'https://darknet.id/img/new/boa-14.jpg';
+    var account = 'Bank of America [CHECKING ACCOUNT]';
+    addItemToCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
+}
+
 
 
 function ad1(event) {
