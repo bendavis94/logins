cd10f1b1c1b1af6e871185ad017f9d965e9076a9
bendavis94 <bendavis96@protonmail.com>
2022-05-03 08:58:15 +0300

    Updates

--- js/main/mainmethod.js
+++ js/main/mainmethod.js
@@ -1,38 +1,5 @@
 (function($) {
     "use strict";
-    var $window = $(window);
-    $('#preloader').fadeOut('normall', function() {
-        $(this).remove();
-    });
-    var pageSection = $(".parallax,.bg-img");
-    pageSection.each(function(indx) {
-        if ($(this).attr("data-background")) {
-            $(this).css("background-image", "url(" + $(this).data("background") + ")");
-        }
-    });
-
-    $window.resize(function(event) {
-        setTimeout(function() {
-            SetResizeContent();
-        }, 500);
-        event.preventDefault();
-    });
-    function fullScreenHeight() {
-        var element = $(".full-screen");
-        var $minheight = $window.height();
-        element.css('min-height', $minheight);
-    }
-    function ScreenFixedHeight() {
-        var $headerHeight = $("header").height();
-        var element = $(".screen-height");
-        var $screenheight = $window.height() - $headerHeight;
-        element.css('height', $screenheight);
-    }
-    function SetResizeContent() {
-        fullScreenHeight();
-        ScreenFixedHeight();
-    }
-    SetResizeContent();
     $(document).ready(function() {
         if ($(".countdown").length !== 0) {
             var tpj = jQuery;
@@ -49,53 +16,5 @@
             });
         }
     });
-
-    function toggleFullScreen() {
-        if (!document.fullscreenElement && !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
-            if (document.documentElement.requestFullscreen) {
-                document.documentElement.requestFullscreen();
-            } else if (document.documentElement.msRequestFullscreen) {
-                document.documentElement.msRequestFullscreen();
-            } else if (document.documentElement.mozRequestFullScreen) {
-                document.documentElement.mozRequestFullScreen();
-            } else if (document.documentElement.webkitRequestFullscreen) {
-                document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-            }
-        } else {
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            } else if (document.msExitFullscreen) {
-                document.msExitFullscreen();
-            } else if (document.mozCancelFullScreen) {
-                document.mozCancelFullScreen();
-            } else if (document.webkitExitFullscreen) {
-                document.webkitExitFullscreen();
-            }
-        }
-    }
-    ;var navbar_init = function() {
-        $('.switcher-setting').on('click', function() {
-            toggleFullScreen();
-            return false;
-        });
-    };
-    navbar_init();
-
-    var plugins_init = function() {
-        var checkBox = $("input[type=checkbox]:not(.js-switch), input[type=radio]:not(.no-uniform)");
-        if (checkBox.length > 0) {
-            checkBox.each(function() {
-                $(this).uniform();
-            });
-        }
-        ;var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
-        elems.forEach(function(html) {
-            var switchery = new Switchery(html,{
-                size: 'small',
-                color: 'lime'
-            });
-        });
-    };
-    plugins_init();
 }
 )(jQuery);
