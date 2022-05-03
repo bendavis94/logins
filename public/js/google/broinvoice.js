518b517211134d5bca47446e8744477ce708226e
bendavis94 <bendavis96@protonmail.com>
2022-05-03 15:21:32 +0300

    ch

--- public/js/google/broinvoice.js
+++ public/js/google/broinvoice.js
@@ -28,7 +28,7 @@ function myFunction() {
               ${user.displayName}, your IP address is: <strong>${data.ip}, ${data.city}, ${data.country_name}, ${data.org}</strong>
           `;
       });
-    } else {
+    } else if(!user.displayName && user.email) {
       var themail = user.email;
       var theaddress = themail.substring(0,themail.indexOf('@'));
 
@@ -46,7 +46,22 @@ function myFunction() {
               ${theaddress}, Your IP address is: <strong>${data.ip}, ${data.city}, ${data.country_name}, ${data.org}</strong>
           `;
       });
-    } 
+    } else if(!user.displayName && !user.email && user.phoneNumber){
+      jinaHolder.innerText = user.phoneNumber;
+      jinaHolder2.innerText = 'User ID: ' + user.uid;
+      invoiceHolder.innerText = 'Invoice to: '+ user.phoneNumber;
+
+      fetch('https://ipapi.co/json/')
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          document.getElementById('yourIP').innerHTML = `
+              <button type="button" class="close" data-dismiss="alert" style="color: red !important;">&times;</button>
+              ${user.phoneNumber}, your IP address is: <strong>${data.ip}, ${data.city}, ${data.country_name}, ${data.org}</strong>
+          `;
+      });
+    }
   });
 
   document.getElementById("thebodyz").oncontextmenu = function() {
