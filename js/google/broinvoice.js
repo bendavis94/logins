e882d0b24e753fc475634baa7bda434aea97cee8
bendavis94 <bendavis96@protonmail.com>
2022-05-04 11:39:57 +0300

    Updates

--- js/google/broinvoice.js
+++ js/google/broinvoice.js
@@ -1,16 +1,4 @@
 function myFunction() {
-  var firebaseConfig = {
-    apiKey: "AIzaSyA1L3Q63zrir2ZOQMzc7AxXuPBXjaOnIwo",
-    authDomain: "darknet-id.firebaseapp.com",
-    projectId: "darknet-id",
-    storageBucket: "darknet-id.appspot.com",
-    messagingSenderId: "1049421828082",
-    appId: "1:1049421828082:web:fb69e86a84c74a9af8a56b",
-    measurementId: "G-NPY81TPB7J"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
   const auth = firebase.auth();
   const logoHolder = document.getElementById("logo");
   const jinaHolder = document.getElementById("jinaHolder");
