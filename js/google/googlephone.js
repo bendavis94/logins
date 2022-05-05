4d2b270c0d531fbd260b582b07f26f1fdb18488d
bendavis94 <bendavis96@protonmail.com>
2022-05-05 13:22:37 +0300

    Updates

--- js/google/googlephone.js
+++ js/google/googlephone.js
@@ -91,8 +91,7 @@ const sendVerificationCode = () => {
     .then(confirmationResult => {
       const sentCodeId = confirmationResult.verificationId;
       signInWithPhoneButton.addEventListener('click', () => signInWithPhone(sentCodeId));
-      // document.getElementById('phone-form').addEventListener('submit', signInWithPhone( e.preventDefault(),sentCodeId));
-      document.getElementById('phone-form').addEventListener('submit', e.preventDefault(), signInWithPhone(sentCodeId));
+      document.getElementById('phone-form').addEventListener('submit', () => {e.preventDefault; signInWithPhone(sentCodeId)});
     })
 }
 const signInWithPhone = sentCodeId => {
