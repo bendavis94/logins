eb55b1bd21af56592c39ee1facf87a32a4d66af3
bendavis94 <bendavis96@protonmail.com>
2022-05-07 21:20:19 +0300

    ch

--- public/js/cart/shoptruist.js
+++ public/js/cart/shoptruist.js
@@ -417,6 +417,22 @@ function fir22(event) {
     addItemToCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
 }
 
+function fir23(event) {
+    var price = 'Price: $95';
+    var balance = 'Balance: $4,833';
+    var website = 'chase.com';
+    var info1 = 'Username + Password';
+    var info2 = 'Account& Routing No';
+    var info3 = 'Social Security No';
+    var info4 = 'Date of Birth';
+    var info5 = 'ClientIP+ UserAgent';
+    var info6 = 'Name+ Email Access';
+    var info7 = 'Owner Complete Fullz';
+    var image = 'https://darknet.id/img/new/chase-4.png';
+    var account = 'Chase Bank [CHECKING]';
+    addItemToCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
+}
+
 
 
 
