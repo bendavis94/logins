6a70cf987be2cff46df6f74341b20bb6617993e0
bendavis94 <bendavis96@protonmail.com>
2022-05-07 20:27:31 +0300

    Updates

--- js/cart/shoptruist.js
+++ js/cart/shoptruist.js
@@ -337,6 +337,21 @@ function fir17(event) {
     addItemToCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
 }
 
+function fir18(event) {
+    var price = 'Price: $76';
+    var balance = 'Balance: $3,800';
+    var website = 'truist.com';
+    var info1 = 'Username + Password';
+    var info2 = 'Account& Routing No';
+    var info3 = 'Social Security No';
+    var info4 = 'Date of Birth';
+    var info5 = 'ClientIP + Useragent';
+    var info6 = 'Name+ Email Access';
+    var info7 = 'Owner\'s Complete Fullz';
+    var image = 'https://darknet.id/img/new/truist-3.8.png';
+    var account = 'Truist Bank [SAVINGS]';
+    addItemToCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
+}
 
 
 
