50c2c3ba7fcc21536a8936e43f56afe22df5166c
bendavis94 <bendavis96@protonmail.com>
2022-05-07 21:36:42 +0300

    ch

--- public/js/cart/shoptruist.js
+++ public/js/cart/shoptruist.js
@@ -497,7 +497,21 @@ function fir26(event) {
     addItemToCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
 }
 
-
+function fir27(event) {
+    var price = 'Price: $129';
+    var balance = 'Balance: $6,041';
+    var website = 'navyfederal.org';
+    var info1 = 'Username + Password';
+    var info2 = 'Account& Routing No';
+    var info3 = 'Social Security No';
+    var info4 = 'Date of Birth';
+    var info5 = 'ClientIP + Useragent';
+    var info6 = 'Name+ Email Access';
+    var info7 = 'Owner\'s Complete Fullz';
+    var image = 'https://darknet.id/img/new/navy-6.0.png';
+    var account = 'Navy Federal [CHECKING]';
+    addItemToCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
+}
 
 
 
