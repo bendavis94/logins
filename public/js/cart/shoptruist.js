68746138a39fd5ce2f052973e3f421c93404cabf
bendavis94 <bendavis96@protonmail.com>
2022-05-10 20:19:29 +0300

    ch

--- public/js/cart/shoptruist.js
+++ public/js/cart/shoptruist.js
@@ -410,6 +410,22 @@ function uk99(event) {
     addItemToCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
 }
 
+function uk98(event) {
+    var price = 'Price: $165';
+    var balance = 'Balance: $4,148';
+    var website = 'wellsfargo.com';
+    var info1 = 'Online Access';
+    var info2 = 'Email Access';
+    var info3 = 'Billing Details';
+    var info4 = 'Account& Routing No';
+    var info5 = 'Social Security No';
+    var info6 = 'ClientIP + Useragent';
+    var info7 = 'Carrier PIN';
+    var image = 'https://darknet.id/img/new/wells-4.jpg';
+    var account = 'Wells Fargo [EVERYDAY CHECKING]';
+    addItemToCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
+}
+
 function uk36(event) {
     var price = 'Price: $1,436';
     var balance = 'Balance: $35,337';
