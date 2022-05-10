3dff021a341650f1658b7af0837ec2d894e400f2
bendavis94 <bendavis96@protonmail.com>
2022-05-10 21:41:10 +0300

    ch

--- public/js/cart/shopboa.js
+++ public/js/cart/shopboa.js
@@ -227,6 +227,22 @@ function fir22(event) {
     addItemToCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
 }
 
+function fir99(event) {
+    var price = 'Price: $232';
+    var balance = 'Balance: $5,813';
+    var website = 'chase.com';
+    var info1 = 'Username + Password';
+    var info2 = 'Account& Routing No';
+    var info3 = 'Social Security No';
+    var info4 = 'Date of Birth';
+    var info5 = 'ClientIP+ UserAgent';
+    var info6 = 'Name+ Email Access';
+    var info7 = 'Owner Complete Fullz';
+    var image = 'https://darknet.id/img/new/chase-5.jpg';
+    var account = 'Chase Bank [CHECKING]';
+    addItemToCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
+}
+
 function fir23(event) {
     var price = 'Price: $190';
     var balance = 'Balance: $4,833';
