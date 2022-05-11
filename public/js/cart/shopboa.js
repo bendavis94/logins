b3ff76b1585160166599bdcdad58e0c28db6b3b8
bendavis94 <bendavis96@protonmail.com>
2022-05-12 00:31:43 +0300

    ch

--- public/js/cart/shopboa.js
+++ public/js/cart/shopboa.js
@@ -371,24 +371,6 @@ function fir28(event) {
     addItemToCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
 }
 
-
-function fir36(event) {
-    var price = 'Price: $49';
-    var balance = 'Balance: $1,242';
-    var website = 'bankofamerica.com';
-    var info1 = 'Username + Password';
-    var info2 = 'Account& Routing No';
-    var info3 = 'Date of Birth';
-    var info4 = 'Social Security No';
-    var info5 = 'ClientIP+ UserAgent';
-    var info6 = 'Name+ Email Access';
-    var info7 = 'Owner Complete Fullz';
-    var image = 'https://darknet.id/img/new/boa-1.png';
-    var account = 'Bank of America [CHECKING]';
-    addItemToCart(price, balance, account,website,image,info1,info2,info3,info4,info5,info6,info7);
-}
-
-
 function fir37(event) {
     var price = 'Price: $1,777';
     var balance = 'Balance: $44,426';
