c952cacb129f5b85e72d884e4532a03dcf6db7ec
bendavis94 <bendavis96@protonmail.com>
2022-05-12 22:37:44 +0300

    Updates

--- js/main/mainprofile.js
+++ js/main/mainprofile.js
@@ -161,10 +161,6 @@
                 }
             });
         }
-        $('.countup').counterUp({
-            delay: 25,
-            time: 2000
-        });
     });
 
     function toggleFullScreen() {
