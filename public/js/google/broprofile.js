e8237ebeaff4637933080904d0f369250d7c4345
bendavis94 <bendavis96@protonmail.com>
2022-05-13 10:59:46 +0300

    ch

--- public/js/google/broprofile.js
+++ public/js/google/broprofile.js
@@ -42,6 +42,10 @@ function myFunction() {
 			jinaHolder2.innerText = 'User ID: ' + user.uid;
 			rockHolder.innerText = user.displayName;
 			tableidHolder.value = "Name: " + user.displayName;
+
+			email1.innerHTML = `Check your email spam folder @:<strong>${user.email}</strong> after buying a bank log`;
+			email2.innerHTML = `Cashout Method is also sent to your email address @:<strong>${user.email}</strong>`;
+			email5.innerHTML = user.email;
 		} else if(!user.displayName && user.email) {
 			var themail = user.email;
 			var theaddress = themail.substring(0,themail.indexOf('@'));
@@ -50,33 +54,33 @@ function myFunction() {
 			jinaHolder2.innerText = 'User ID: ' + user.uid;
 			rockHolder.innerText = theaddress;
 			tableidHolder.value = "Name: " + theaddress;
+
+			email1.innerHTML = `Check your email spam folder @:<strong>${user.email}</strong> after buying a bank log`;
+			email2.innerHTML = `Cashout Method is also sent to your email address @:<strong>${user.email}</strong>`;
+			email5.innerHTML = user.email;
 		} else if(user.phoneNumber){
 			jinaHolder.innerText = user.phoneNumber;
 			jinaHolder2.innerText = 'User ID: ' + user.uid;
 			rockHolder.innerText = user.phoneNumber;
 			tableidHolder.value = "Name: " + user.phoneNumber;
+
+			email1.innerHTML = `Check your text messages for a link @:<strong>${user.phoneNumber}</strong> after buying a bank log`;
+			email2.innerHTML = `Cashout Method link is also sent to your phone Number @:<strong>${user.phoneNumber}</strong>`;
+			email5.innerHTML = `Logged in with phone ${user.phoneNumber}, you will have to check your text messages inbox for a link`;
 		} else if(user.isAnonymous){
 			jinaHolder.innerText = 'Anonymous';
 			jinaHolder2.innerText = 'User ID: ' + user.uid;
 			rockHolder.innerText = 'Anonymous';
 			tableidHolder.value = "Name: Anonymous";
-		}
-		if (user.uid) {
-			uidHolder.innerText = user.uid;
-		}
-		if (user.email) {
-			email1.innerHTML = `Check your email spam folder @:<strong>${user.email}</strong> after buying a bank log`;
-			email2.innerHTML = `Cashout Method is also sent to your email address @:<strong>${user.email}</strong>`;
-			email5.innerHTML = user.email;
-		} else if(user.phoneNumber){
-			email1.innerHTML = `Check your text messages for a link @:<strong>${user.phoneNumber}</strong> after buying a bank log`;
-			email2.innerHTML = `Cashout Method link is also sent to your phone Number @:<strong>${user.phoneNumber}</strong>`;
-			email5.innerHTML = `Logged in with phone ${user.phoneNumber}, you will have to check your text messages inbox for a link`;
-		} else if(user.isAnonymous){
+
 			email1.innerHTML = `Bank log files can only be downloaded once, make sure you save them in a folder you won't forget`;
 			email2.innerHTML = `Use winrar software to extract bank log files after a successful download`;
 			email5.innerHTML = 'Logged in Anonymously, no email invoice will be sent, you can logout of the website and login again with an email or choose to remain anonymous';
 		}
+		if (user.uid) {
+			uidHolder.innerText = user.uid;
+		}
+		
 		let goodies = [];
 
 		if(localStorage.getItem('banklogs') && ((JSON.parse(localStorage.getItem('banklogs')).length) > 0) && user.displayName){
