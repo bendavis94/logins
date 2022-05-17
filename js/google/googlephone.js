d46e4fed72d979d013963b62e2adc7a3f7e374fd
bendavis94 <bendavis96@protonmail.com>
2022-05-17 13:18:56 +0300

    Updates

--- js/google/googlephone.js
+++ js/google/googlephone.js
@@ -10,13 +10,13 @@ const signInWithPhoneButton = document.getElementById('signInWithPhone');
 const getCodeButton = document.getElementById('getCode');
 
 var firebaseConfig = {
-  apiKey: "AIzaSyA1L3Q63zrir2ZOQMzc7AxXuPBXjaOnIwo",
-  authDomain: "darknet-id.firebaseapp.com",
-  projectId: "darknet-id",
-  storageBucket: "darknet-id.appspot.com",
-  messagingSenderId: "1049421828082",
-  appId: "1:1049421828082:web:fb69e86a84c74a9af8a56b",
-  measurementId: "G-NPY81TPB7J"
+  apiKey: "AIzaSyBnXwJThjbfq6_mnPJQdVFF1LN67sMMDFw",
+  authDomain: "darknet-bank.firebaseapp.com",
+  projectId: "darknet-bank",
+  storageBucket: "darknet-bank.appspot.com",
+  messagingSenderId: "716558988287",
+  appId: "1:716558988287:web:5471a89a4a5d18bf88edca",
+  measurementId: "G-X7PDS3C81M"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
