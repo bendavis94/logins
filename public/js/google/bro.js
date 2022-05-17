fe7cdcca33cb3da9de505fe34909339bf528a617
bendavis94 <bendavis96@protonmail.com>
2022-05-17 13:21:44 +0300

    ch

--- public/js/google/bro.js
+++ public/js/google/bro.js
@@ -1,12 +1,12 @@
 function myFunction() {
   var firebaseConfig = {
-    apiKey: "AIzaSyBnXwJThjbfq6_mnPJQdVFF1LN67sMMDFw",
-    authDomain: "darknet-bank.firebaseapp.com",
-    projectId: "darknet-bank",
-    storageBucket: "darknet-bank.appspot.com",
-    messagingSenderId: "716558988287",
-    appId: "1:716558988287:web:5471a89a4a5d18bf88edca",
-    measurementId: "G-X7PDS3C81M"
+    apiKey: "AIzaSyA1L3Q63zrir2ZOQMzc7AxXuPBXjaOnIwo",
+    authDomain: "darknet-id.firebaseapp.com",
+    projectId: "darknet-id",
+    storageBucket: "darknet-id.appspot.com",
+    messagingSenderId: "1049421828082",
+    appId: "1:1049421828082:web:fb69e86a84c74a9af8a56b",
+    measurementId: "G-NPY81TPB7J"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
