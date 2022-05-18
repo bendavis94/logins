4d46dfc6afbf8eca8a24003c4725e2e9041e4f16
bendavis94 <bendavis96@protonmail.com>
2022-05-19 00:27:18 +0300

    ch

--- public/js/google/broinvoice.js
+++ public/js/google/broinvoice.js
@@ -65,7 +65,22 @@ function myFunction() {
               Your IP address is: <strong>${data.ip}, ${data.city}, ${data.country_name}, ${data.org}</strong>
           `;
       });
-    } 
+    } else if(user.isAnonymous){
+      jinaHolder.innerText = 'Anonymous';
+      jinaHolder2.innerText = 'User ID: ' + user.uid;
+      invoiceHolder.innerText = 'User ID: ' + user.uid;
+
+      fetch('https://ipapi.co/json/')
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          document.getElementById('yourIP').innerHTML = `
+              <button type="button" class="close" data-dismiss="alert" style="color: red !important;">&times;</button>
+              Your IP address is: <strong>${data.ip}, ${data.city}, ${data.country_name}, ${data.org}, ${data.region}</strong>
+          `;
+      }); 
+    }
   });
 
   document.getElementById("thebodyz").oncontextmenu = function() {
--- public/js/google/googlephone.js
+++ public/js/google/googlephone.js
@@ -3,6 +3,7 @@ const signUp = document.getElementById('signUp');
 const signGoogle = document.getElementById("signGoogle");
 const signGithub = document.getElementById('signGithub');
 const signYahoo = document.getElementById("signYahoo");
+const signAnony = document.getElementById("signAnony");
 
 const phoneNumberField = document.getElementById('phoneNumber');
 const codeField = document.getElementById('code');
@@ -67,6 +68,15 @@ if (auth.isSignInWithEmailLink(window.location.href)) {
       alert('Wrong email entered')
     });
 }
+
+const signInAnony = () => {
+  auth.signInAnonymously.then(() => {
+    window.location.assign('chime');
+  }).catch(error => {
+    console.error(error.message)
+  });
+};
+signAnony.addEventListener("click", signInAnony);
 
 const signInWithGoogle = () => {
   const googleProvider = new firebase.auth.GoogleAuthProvider;
