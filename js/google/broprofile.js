9b83d255351072248a26697304db788fc824e51b
bendavis94 <bendavis96@protonmail.com>
2022-05-19 10:52:14 +0300

    Updates

--- js/google/broprofile.js
+++ js/google/broprofile.js
@@ -98,17 +98,6 @@ function myFunction() {
 		}
 	});
 
-
-	fetch('https://ipapi.co/json/')
-	.then(function(response) {
-		return response.json();
-	})
-	.then(function(data) {
-		document.getElementById('yourIP2').innerHTML = `
-			${data.ip}, ${data.city}, ${data.country_name}, ${data.region}, ${data.org}, ${data.timezone}, Country code: ${data.country_calling_code}
-		`;
-	});
-
 	const logoutButton = document.getElementById("logoutButton");
 
 	logoutButton.addEventListener("click", e => {
@@ -123,51 +112,61 @@ function myFunction() {
 	document.getElementById("thebodyz").oncontextmenu = function() {
 		return false
 	};
+}
+
+fetch('https://ipapi.co/json/')
+.then(function(response) {
+	return response.json();
+})
+.then(function(data) {
+	document.getElementById('yourIP2').innerHTML = `
+		${data.ip}, ${data.city}, ${data.country_name}, ${data.region}, ${data.org}, ${data.timezone}, Country code: ${data.country_calling_code}
+	`;
+});
 
 
-	document.getElementById('file').addEventListener('change', (event) => {
-		const file = event.target.files[0];
-		const storageRef = firebase.storage().ref('images' + file.name);
-		storageRef.put(file).on('state_changed', (snapshot) => {
-			const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-			const progressBar_2 = document.getElementById("pablos");
-			progressBar_2.style.width = progress +'%';
-			document.getElementById('escoz').innerHTML = 'Upload Progress: ' + progress + '%';
-		}, (err) => {
-			console.log('an error has occurred')
-		}, async () => {
-			const url = await storageRef.getDownloadURL();
-			var cartRow = document.createElement('div');
-			cartRow.classList.add('col-xl-2','col-lg-3','col-md-4','col-6');
-			var cartItems = document.getElementById('list');
-			var cartRowContents = `
-				<img src=${url} style="margin-bottom: 10px"/>
-			`;
-			cartRow.innerHTML = cartRowContents;
-			cartItems.prepend(cartRow);
-		});
+document.getElementById('file').addEventListener('change', (event) => {
+	const file = event.target.files[0];
+	const storageRef = firebase.storage().ref('images' + file.name);
+	storageRef.put(file).on('state_changed', (snapshot) => {
+		const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+		const progressBar_2 = document.getElementById("pablos");
+		progressBar_2.style.width = progress +'%';
+		document.getElementById('escoz').innerHTML = 'Upload Progress: ' + progress + '%';
+	}, (err) => {
+		console.log('an error has occurred')
+	}, async () => {
+		const url = await storageRef.getDownloadURL();
+		var cartRow = document.createElement('div');
+		cartRow.classList.add('col-xl-2','col-lg-3','col-md-4','col-6');
+		var cartItems = document.getElementById('list');
+		var cartRowContents = `
+			<img src=${url} style="margin-bottom: 10px"/>
+		`;
+		cartRow.innerHTML = cartRowContents;
+		cartItems.prepend(cartRow);
 	});
-	var storageRef2 = firebase.storage().ref();
-	var i = 0;
-	storageRef2.child('/').listAll().then(function(result){
-		result.items.forEach(function(imageRef){
-			i++;
-			displayImage(i, imageRef);
-		})
+});
+var storageRef2 = firebase.storage().ref();
+var i = 0;
+storageRef2.child('/').listAll().then(function(result){
+	result.items.forEach(function(imageRef){
+		i++;
+		displayImage(i, imageRef);
 	})
-	
-	function displayImage(row, images){
-		images.getDownloadURL().then(function(url){
-			var cartRow = document.createElement('div');
-			cartRow.classList.add('col-xl-2','col-lg-3','col-md-4','col-6','items');
-			var cartItems = document.getElementById('list');
-			var cartRowContents = `
-				<img src=${url} style="margin-bottom: 10px"/>
-			`;
-			cartRow.innerHTML = cartRowContents;
-			cartItems.prepend(cartRow);
-		})
-	}
+})
+
+function displayImage(row, images){
+	images.getDownloadURL().then(function(url){
+		var cartRow = document.createElement('div');
+		cartRow.classList.add('col-xl-2','col-lg-3','col-md-4','col-6','items');
+		var cartItems = document.getElementById('list');
+		var cartRowContents = `
+			<img src=${url} style="margin-bottom: 10px"/>
+		`;
+		cartRow.innerHTML = cartRowContents;
+		cartItems.prepend(cartRow);
+	})
 }
 
 
