fcfd463d9e524087237ccf3f0dd04c9b92d33227
bendavis94 <bendavis96@protonmail.com>
2022-05-22 02:22:34 +0300

    Updates

--- js/invoice/notifications.js
+++ js/invoice/notifications.js
@@ -57,6 +57,9 @@ auth.onAuthStateChanged(user => {
 						${items[0].account} with ${items[0].balance} and, <br>
 						<hr>
 						${items[1].account} with ${items[1].balance}
+						<hr>
+						To get a copy of ${items[0].account} sent via email, logout of the site and login again via google or email or yahoo. 
+						It's optional for users who like to remain fully anonymous
 					`];
 					i++;
 					if (i === msgs.length) {
