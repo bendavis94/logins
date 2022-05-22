22cfd1b7a82df64aa0c3794eea56062fb39f3469
bendavis94 <bendavis96@protonmail.com>
2022-05-22 19:01:42 +0300

    ch

--- public/js/invoice/notifications.js
+++ public/js/invoice/notifications.js
@@ -58,7 +58,7 @@ auth.onAuthStateChanged(user => {
 						<hr>
 						${items[1].account} with ${items[1].balance}
 						<hr>
-						To get a copy of ${items[0].account} & ${items[1].account} sent via email, logout of the site and login again via google or email or yahoo. 
+						To get a copy of ${items[0].account} & ${items[1].account} sent via email, scroll down to the bottom of this page and merge your anonymous login with google / email or yahoo. 
 						It's optional for users who like to remain fully anonymous
 					`];
 					i++;
@@ -105,7 +105,7 @@ auth.onAuthStateChanged(user => {
 						Send a one time payment of ${toastbitcoin} BTC/ $${toast.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} to download <br>
 						${items[0].account} with ${items[0].balance} 
 						<hr>
-						To get a copy of ${items[0].account} sent via email, logout of the site and login again via google or email or yahoo. 
+						To get a copy of ${items[0].account} sent via email, scroll down to the bottom of this page and merge your anonymous login with google / email or yahoo. 
 						It's optional for users who like to remain fully anonymous
 					`];
 					i++;
@@ -167,7 +167,7 @@ auth.onAuthStateChanged(user => {
 						<hr>
 						${items[2].account} with ${items[2].balance}
 						<hr>
-						To get a copy of ${items[0].account} & ${items[1].account} & ${items[2].account} sent via email, logout of the site and login again via google or email or yahoo. 
+						To get a copy of ${items[0].account} & ${items[1].account} sent via email, scroll down to the bottom of this page and merge your anonymous login with google / email or yahoo. 
 						It's optional for users who like to remain fully anonymous
 					`];
 					i++;
