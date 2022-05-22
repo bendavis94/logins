ea15fc9cb0850570d46780fc9367840df6b4f5fd
bendavis94 <bendavis96@protonmail.com>
2022-05-22 18:59:10 +0300

    c

--- public/js/google/broinvoice7.js
+++ public/js/google/broinvoice7.js
@@ -38,7 +38,7 @@ var firebaseConfig = {
     event.preventDefault();
     const email = mailField.value;
     var actionCodeSettings = {
-        url: 'https://darknet.id/invoice7',
+        url: 'https://darknet.id/invoice',
         handleCodeInApp: true,
     };
     auth.sendSignInLinkToEmail(email, actionCodeSettings)
@@ -62,7 +62,7 @@ var firebaseConfig = {
     auth.signInWithEmailLink(email, window.location.href)
       .then((result) => {
         sendVerificationEmail();
-        location.href = 'https://darknet.id/invoice7';
+        location.href = 'https://darknet.id/invoice';
       })
       .catch((error) => {
         console.log(error.message)
