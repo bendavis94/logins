326e7e05ee8f21cf186a6f8ad89f350ad1edb91f
bendavis94 <bendavis96@protonmail.com>
2022-05-23 22:02:51 +0300

    ch

--- public/js/google/bro.js
+++ public/js/google/bro.js
@@ -15,6 +15,9 @@ const jinaHolder = document.getElementById("jinaHolder");
 const jinaHolder2 = document.getElementById("jinaHolder2");
 const emailInbox = document.getElementById("email-inbox");
 
+const emailLogin = document.getElementById('mail-login');
+const anonyLogin = document.getElementById('anony-login');
+
 
 const auth = firebase.auth();
 
@@ -30,20 +33,24 @@ auth.onAuthStateChanged(user => {
     jinaHolder.innerText = user.displayName;
     jinaHolder2.innerText = 'USER ID: ' + user.uid;
     emailInbox.innerHTML = `Check your email inbox <span>${user.email}</span> after buying any bank log.`;
+    emailLogin.style.direction = 'block';
   } else if(!user.displayName && user.email) {
     var themail = user.email;
     var theaddress = themail.substring(0,themail.indexOf('@'));
     jinaHolder.innerText = theaddress;
     jinaHolder2.innerText = 'USER ID: ' + user.uid;
     emailInbox.innerHTML = `Check your email inbox <span>${user.email}</span> after buying any bank log.`;
+    emailLogin.style.display = 'block';
   } else if(user.phoneNumber){
     jinaHolder.innerText = user.phoneNumber;
     jinaHolder2.innerText = 'USER ID: ' + user.uid;
     emailInbox.innerHTML = `Check your text messages inbox <span>${user.phoneNumber}</span> for a link after buying any bank log.`;
+    emailLogin.style.display = 'block';
   } else if(user.isAnonymous){
     jinaHolder.innerText = 'Anonymous';
     jinaHolder2.innerText = 'USER ID: ' + user.uid;
     emailInbox.innerHTML = `Bank log files can only be downloaded <span>ONCE</span>, save them in a folder you won't forget`;
+    anonyLogin.style.display = 'block'
   } 
 });
 
