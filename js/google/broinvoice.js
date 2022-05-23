fd9f07c6f66007d809a18e333e6db454f47be33e
bendavis94 <bendavis96@protonmail.com>
2022-05-23 06:52:19 +0300

    Updates

--- js/google/broinvoice.js
+++ js/google/broinvoice.js
@@ -22,6 +22,16 @@ var firebaseConfig = {
   const signYahoo = document.getElementById('signYahoo');
   const signGithub = document.getElementById("signGithub");
 
+  const sendVerificationEmail = () => {
+    auth.currentUser.sendEmailVerification()
+    .then(() => {
+        alert('Check Verification Link sent to your email')
+    })
+    .catch(error => {
+        console.error(error.message);
+    })
+  }
+
   const signInWithGoogle = () => {
     const googleProvider = new firebase.auth.GoogleAuthProvider;
     auth.signInWithPopup(googleProvider).then(() => {
