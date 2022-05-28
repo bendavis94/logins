768f119e32b14a3016b270b854bc8d780b57201e
bendavis94 <bendavis96@protonmail.com>
2022-05-28 09:12:37 +0300

    Updates

--- js/google/broblog.js
+++ js/google/broblog.js
@@ -35,7 +35,13 @@ auth.onAuthStateChanged(user => {
 	} else if(user.phoneNumber){
 		jinaHolder.innerText = user.phoneNumber;
 		jinaHolder2.innerText = 'User ID: ' + user.uid;
-  } 
+  } else if(user.isAnonymous && user.displayName){
+    jinaHolder.innerText = user.displayName;
+    jinaHolder2.innerText = 'USER ID: ' + user.uid;
+  } else if(user.isAnonymous && !user.displayName){
+    jinaHolder.innerText = 'Anonymous';
+    jinaHolder2.innerText = 'USER ID: ' + user.uid;
+  }
 });
 
 const logoutButton = document.getElementById("logoutButton");
