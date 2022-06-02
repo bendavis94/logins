a634c39b0d59066bfcce26b2047f3ac73a797016
bendavis94 <bendavis96@protonmail.com>
2022-06-03 02:45:16 +0300

    Updates

--- js/google/broinvoice.js
+++ js/google/broinvoice.js
@@ -43,6 +43,18 @@ auth.onAuthStateChanged(user => {
     jinaHolder.innerText = user.phoneNumber;
     jinaHolder2.innerText = 'User ID: ' + user.uid;
     invoiceHolder.innerText = 'Invoice to: '+ user.phoneNumber;
+  } else if(user.isAnonymous && user.displayName){
+    jinaHolder.innerText = user.displayName;
+    jinaHolder2.innerText = 'User ID: ' + user.uid;
+  } else if(user.isAnonymous && !user.displayName){
+    jinaHolder.innerText = 'Anonymous';
+    jinaHolder2.innerText = 'User ID: ' + user.uid;
+  } else if(user.isAnonymous && user.displayName){
+    jinaHolder.innerText = user.displayName;
+    jinaHolder2.innerText = 'User ID: ' + user.uid;
+  } else if(user.isAnonymous && !user.displayName){
+    jinaHolder.innerText = 'Anonymous';
+    jinaHolder2.innerText = 'User ID: ' + user.uid;
   }
 });
 
@@ -57,6 +69,17 @@ fetch('https://ipapi.co/json/')
   `;
 });
 
+
+const logoutButton = document.getElementById("logoutButton");
+
+logoutButton.addEventListener("click", e => {
+    e.preventDefault();
+    auth.signOut().then(() => {
+        window.location.assign("index");
+    }).catch(error => {
+        console.error(error);
+    });
+});
 document.getElementById("thebodyz").oncontextmenu = function() {
   return false
 };
