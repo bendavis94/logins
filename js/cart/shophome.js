624a0da497c2ad66fb189ac9e0b75723aeef909a
bendavis94 <bendavis96@protonmail.com>
2022-06-03 16:59:07 +0300

    Updates

--- js/cart/shophome.js
+++ js/cart/shophome.js
@@ -1,10 +1,10 @@
 let items = [];
 
 var month = new Array();
-month[4] = "May";
 month[5] = "June";
 month[6] = "July";
 month[7] = "August";
+month[8] = "Septemeber";
 var d = new Date();
 var n = month[d.getMonth()];
 var y = d.getFullYear();
