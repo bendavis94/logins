614bb8b9803df48b4c2e046d7073f8ca8bc536bb
bendavis94 <bendavis96@protonmail.com>
2022-06-04 10:38:16 +0300

    Updates

--- js/google/bro.js
+++ js/google/bro.js
@@ -37,13 +37,7 @@ auth.onAuthStateChanged(user => {
   } else if(user.phoneNumber && !user.displayName){
     jinaHolder.innerText = user.phoneNumber;
     jinaHolder2.innerText = 'User ID: ' + user.uid;
-  } else if(user.isAnonymous && user.displayName){
-    jinaHolder.innerText = user.displayName;
-    jinaHolder2.innerText = 'User ID: ' + user.uid;
-  } else if(user.isAnonymous && !user.displayName){
-    jinaHolder.innerText = 'Anonymous';
-    jinaHolder2.innerText = 'User ID: ' + user.uid;
-  }
+  } 
 });
 
 fetch('https://ipapi.co/json/')
